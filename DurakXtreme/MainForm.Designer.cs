aa3c002b3a5e2ccef66d936e1ca3434725d0b926
Dylan Lopez <dylan.lopez@dcmail.ca>
2019-03-25 13:03:52 -0400

    Added GUI

---
+++ DurakXtreme/MainForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace DurakXtreme
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlOpponent = new System.Windows.Forms.Panel();
+            this.pnlPlayerOne = new System.Windows.Forms.Panel();
+            this.lblPlayerOne = new System.Windows.Forms.Label();
+            this.lblOpponent = new System.Windows.Forms.Label();
+            this.pbDeck = new System.Windows.Forms.PictureBox();
+            this.pnlPlayArea = new System.Windows.Forms.Panel();
+            this.DurakXtremeTips = new System.Windows.Forms.ToolTip(this.components);
+            this.pnlGraveyard = new System.Windows.Forms.Panel();
+            this.btnTake = new System.Windows.Forms.Button();
+            this.btnPass = new System.Windows.Forms.Button();
+            this.lblStatusUpdate = new System.Windows.Forms.Label();
+            this.lblCardCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDeck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlOpponent
+            // 
+            this.pnlOpponent.AllowDrop = true;
+            this.pnlOpponent.BackColor = System.Drawing.Color.RoyalBlue;
+            this.pnlOpponent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlOpponent.Location = new System.Drawing.Point(255, 12);
+            this.pnlOpponent.Name = "pnlOpponent";
+            this.pnlOpponent.Size = new System.Drawing.Size(529, 134);
+            this.pnlOpponent.TabIndex = 0;
+            // 
+            // pnlPlayerOne
+            // 
+            this.pnlPlayerOne.AllowDrop = true;
+            this.pnlPlayerOne.BackColor = System.Drawing.Color.Orange;
+            this.pnlPlayerOne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlPlayerOne.Location = new System.Drawing.Point(255, 469);
+            this.pnlPlayerOne.Name = "pnlPlayerOne";
+            this.pnlPlayerOne.Size = new System.Drawing.Size(529, 134);
+            this.pnlPlayerOne.TabIndex = 1;
+            // 
+            // lblPlayerOne
+            // 
+            this.lblPlayerOne.AutoSize = true;
+            this.lblPlayerOne.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayerOne.ForeColor = System.Drawing.Color.Snow;
+            this.lblPlayerOne.Location = new System.Drawing.Point(255, 450);
+            this.lblPlayerOne.Name = "lblPlayerOne";
+            this.lblPlayerOne.Size = new System.Drawing.Size(45, 13);
+            this.lblPlayerOne.TabIndex = 2;
+            this.lblPlayerOne.Text = "Player 1";
+            // 
+            // lblOpponent
+            // 
+            this.lblOpponent.AutoSize = true;
+            this.lblOpponent.BackColor = System.Drawing.Color.Transparent;
+            this.lblOpponent.ForeColor = System.Drawing.Color.Snow;
+            this.lblOpponent.Location = new System.Drawing.Point(748, 153);
+            this.lblOpponent.Name = "lblOpponent";
+            this.lblOpponent.Size = new System.Drawing.Size(45, 13);
+            this.lblOpponent.TabIndex = 3;
+            this.lblOpponent.Text = "Player 2";
+            // 
+            // pbDeck
+            // 
+            this.pbDeck.BackColor = System.Drawing.Color.Transparent;
+            this.pbDeck.Location = new System.Drawing.Point(95, 245);
+            this.pbDeck.Name = "pbDeck";
+            this.pbDeck.Size = new System.Drawing.Size(75, 108);
+            this.pbDeck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbDeck.TabIndex = 4;
+            this.pbDeck.TabStop = false;
+            // 
+            // pnlPlayArea
+            // 
+            this.pnlPlayArea.AllowDrop = true;
+            this.pnlPlayArea.BackColor = System.Drawing.Color.Transparent;
+            this.pnlPlayArea.Location = new System.Drawing.Point(255, 222);
+            this.pnlPlayArea.Name = "pnlPlayArea";
+            this.pnlPlayArea.Size = new System.Drawing.Size(529, 167);
+            this.pnlPlayArea.TabIndex = 5;
+            // 
+            // pnlGraveyard
+            // 
+            this.pnlGraveyard.BackColor = System.Drawing.Color.Transparent;
+            this.pnlGraveyard.Location = new System.Drawing.Point(810, 222);
+            this.pnlGraveyard.Name = "pnlGraveyard";
+            this.pnlGraveyard.Size = new System.Drawing.Size(200, 167);
+            this.pnlGraveyard.TabIndex = 6;
+            // 
+            // btnTake
+            // 
+            this.btnTake.BackColor = System.Drawing.Color.Orange;
+            this.btnTake.FlatAppearance.BorderSize = 0;
+            this.btnTake.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTake.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTake.Location = new System.Drawing.Point(322, 408);
+            this.btnTake.Name = "btnTake";
+            this.btnTake.Size = new System.Drawing.Size(116, 30);
+            this.btnTake.TabIndex = 7;
+            this.btnTake.Text = "Take";
+            this.btnTake.UseVisualStyleBackColor = false;
+            // 
+            // btnPass
+            // 
+            this.btnPass.BackColor = System.Drawing.Color.Orange;
+            this.btnPass.FlatAppearance.BorderSize = 0;
+            this.btnPass.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPass.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPass.Location = new System.Drawing.Point(580, 408);
+            this.btnPass.Name = "btnPass";
+            this.btnPass.Size = new System.Drawing.Size(116, 30);
+            this.btnPass.TabIndex = 8;
+            this.btnPass.Text = "Pass";
+            this.btnPass.UseVisualStyleBackColor = false;
+            // 
+            // lblStatusUpdate
+            // 
+            this.lblStatusUpdate.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatusUpdate.Location = new System.Drawing.Point(255, 165);
+            this.lblStatusUpdate.Name = "lblStatusUpdate";
+            this.lblStatusUpdate.Size = new System.Drawing.Size(529, 42);
+            this.lblStatusUpdate.TabIndex = 9;
+            // 
+            // lblCardCount
+            // 
+            this.lblCardCount.AutoSize = true;
+            this.lblCardCount.BackColor = System.Drawing.Color.Transparent;
+            this.lblCardCount.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCardCount.ForeColor = System.Drawing.Color.Gray;
+            this.lblCardCount.Location = new System.Drawing.Point(108, 209);
+            this.lblCardCount.Name = "lblCardCount";
+            this.lblCardCount.Size = new System.Drawing.Size(49, 33);
+            this.lblCardCount.TabIndex = 10;
+            this.lblCardCount.Text = "00";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::DurakXtreme.Properties.Resources.texturedBackground;
+            this.ClientSize = new System.Drawing.Size(1022, 626);
+            this.Controls.Add(this.lblCardCount);
+            this.Controls.Add(this.lblStatusUpdate);
+            this.Controls.Add(this.btnPass);
+            this.Controls.Add(this.btnTake);
+            this.Controls.Add(this.pnlGraveyard);
+            this.Controls.Add(this.pnlPlayArea);
+            this.Controls.Add(this.pbDeck);
+            this.Controls.Add(this.lblOpponent);
+            this.Controls.Add(this.lblPlayerOne);
+            this.Controls.Add(this.pnlPlayerOne);
+            this.Controls.Add(this.pnlOpponent);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Durak Xtreme";
+            ((System.ComponentModel.ISupportInitialize)(this.pbDeck)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlOpponent;
+        private System.Windows.Forms.Panel pnlPlayerOne;
+        private System.Windows.Forms.Label lblPlayerOne;
+        private System.Windows.Forms.Label lblOpponent;
+        private System.Windows.Forms.PictureBox pbDeck;
+        private System.Windows.Forms.Panel pnlPlayArea;
+        private System.Windows.Forms.ToolTip DurakXtremeTips;
+        private System.Windows.Forms.Panel pnlGraveyard;
+        private System.Windows.Forms.Button btnTake;
+        private System.Windows.Forms.Button btnPass;
+        private System.Windows.Forms.Label lblStatusUpdate;
+        private System.Windows.Forms.Label lblCardCount;
+    }
+}
+
---
+++ MainMenu/MainMenuForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MainMenu
+{
+    partial class MainMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(231, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(551, 207);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(385, 324);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(234, 61);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.Location = new System.Drawing.Point(385, 404);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(234, 61);
+            this.btnSettings.TabIndex = 3;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(385, 482);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(234, 61);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // MainMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MainMenu.Properties.Resources.background1;
+            this.ClientSize = new System.Drawing.Size(1022, 626);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainMenuForm";
+            this.Text = "Durak Xtreme";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
