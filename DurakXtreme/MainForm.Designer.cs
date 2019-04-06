05a6cc84802abe64def84fe3213626ab0d4c87ed
Dylan Lopez <dylan.lopez94@gmail.com>
2019-04-06 14:40:34 -0400

    Made the MainForm not resizable

--- DurakXtreme/MainForm.Designer.cs
+++ DurakXtreme/MainForm.Designer.cs
@@ -195,8 +195,10 @@
             this.Controls.Add(this.pnlPlayerOne);
             this.Controls.Add(this.pnlOpponent);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Durak Xtreme";
             ((System.ComponentModel.ISupportInitialize)(this.pbDeck)).EndInit();
